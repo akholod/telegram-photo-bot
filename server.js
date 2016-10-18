4b455bc5dbb14ec781427add8d22e8845d6fe661
Andrey <andrey.kokhanevich@gmail.com>
2016-10-18 13:46:54 +0000

    init commit

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-const bot = require("app");
-var express = require('express');
-var packageInfo = require('./package.json');
-
+const bot = require("./app.js");
+const express = require('express');
+const packageInfo = require('./package.json');
+var port = 8080;
 var app = express();
 
 app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
 
-var server = app.listen(process.env.PORT, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Web server started at http://%s:%s', host, port);
+app.listen(process.env.PORT || port, () => {
+  console.log('App listening on port 8080!');
 });
 
