1751cc092d80e5462c8860356591b1b518987364
Andrey <andrey.kokhanevich@gmail.com>
2016-10-20 10:44:45 +0000

    add new messages

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
+const createMessageHelper = require("./create_message_helper");
 const unirest = require('unirest');
 const token = '289118113:AAGZfNMD_ziSF5HRdhLK3DHBDoPbIsiSJ34';
 
@@ -7,26 +8,21 @@ const bot = new TelegramBot(token, {polling: true});
 bot.on('message', function (msg) {
     'use strict'
     //console.log(msg);
-    let chatId = msg.chat.id;
+    var chatId = msg.chat.id;
     if(!msg.document  && !msg.photo) {
         return bot.sendMessage(chatId,'Это ты мне не фото отпрвил, отправь фото');
     }
     
-    let fileId;
+    var fileId;
     if(msg.photo){
         fileId = msg.photo[0].file_id;
     } else {
         fileId = msg.document.file_id;
     }
     
-    let fileObj = bot.getFileLink(fileId);
+    var fileObj = bot.getFileLink(fileId);
     fileObj.then((link) => {
-        var dataFace = {
-            age: '',
-            gender: '',
-            race: '',
-            smiling: ''
-        };
+       
         console.log('This is file link ' + link);
         bot.sendMessage(chatId,'Подожди, обрабатываю твою фотку ' + link);
         
@@ -35,9 +31,13 @@ bot.on('message', function (msg) {
             .header("Accept", "application/json")
             .end(function (result) {
                 if(!result.body.face[0]) {
-                    return bot.sendMessage(chatId,'У тебя нет лица на фото');
+                    return bot.sendMessage(chatId,'У тебя нет лица на фото, или фото плохого качества. Попробуй отправить его мне как файл.');
                 }
-                console.log(result.body.face[0].attribute.age.value);
+            var messages = createMessageHelper(result.body.face[0]);  
+            console.log(result.body.face[0]);
+            messages.forEach((item) => {
+                 bot.sendMessage(chatId, item);
+            })
         });
     })
     
--- create_message_helper/index.js
+++ create_message_helper/index.js
@@ -1,5 +1,6 @@
 
-function createResultMessege (obj) {
+module.exports = function createResultMessege (obj) {
+    'use strict'
     let resMessages = [];
     let age,
         gender,
@@ -10,22 +11,35 @@ function createResultMessege (obj) {
     age = obj.attribute.age.value;
     gender = obj.attribute.gender.value
     genderConfidence = obj.attribute.gender.confidence;
+    raceConfidence = obj.attribute.race.confidence;
+    smiling = obj.attribute.smiling.value;
     
-    resMessages.push(`Ваш возраст скорее всего ${age  + ageEnding(age)}`);
+    resMessages.push(`Вы улыбаетесь на ${smiling} %`);
+    resMessages.push(`Ваш расса ${getRace(race)} на ${raceConfidence}`);
     resMessages.push(`Ваш пол ${getGender(gender)} на ${genderConfidence}`);
-    
-    
+    resMessages.push(`Ваш возраст скорее всего ${age  + ageEnding(age)}`);
     return resMessages;
+}
+
+function getRace(race) {
+    if(race == 'Asian') {
+        return 'азиатская';
+    }
+    if(race == 'Black') {
+        return 'черная';
+    }
+    return 'верная'
 }
 
 function getGender(gender) {
     if(gender === 'Female') {
-        return 'женщинский';
+        return 'женский';
     }
     return 'мужской';
 }
 
 function ageEnding(age) {
+    'use strict'
     let wordAge;
     if (age > 19) {
         age = age % 10;
